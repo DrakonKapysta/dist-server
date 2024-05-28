924f54feb2b64a6c47625c06fd47dcc04b8bde8d
DrakonKapysta <matyiokin2002@gmail.com>
2024-05-28 14:14:22 +0300

    cleared comments and logs

--- LoadBalancerWRR.js
+++ LoadBalancerWRR.js
@@ -55,7 +55,7 @@ class LoadBlancerWRR {
       .emit('request:task', task, (responce) => {
         if (responce.status == 'ok') {
           this.workers[index].requests.totalSuccessfulRequests += 1;
-          //console.log(responce.payload);
+          console.log(responce.payload);
         }
         if (responce.status == 'error') {
           this.workers[index].requests.totalErrorRequests += 1;
--- functions/execute.js
+++ functions/execute.js
@@ -1,15 +1,6 @@
 module.exports = async function execute(balancer) {
-  //console.log('Set timeout start');
   await balancer.WRR();
-  // for (let worker of balancer.workers) {
-  //   for (let task of worker.taskQueue) {
-  //     console.log(task);
-  //   }
-  //   console.log();
-  //   console.log('===============================================');
-  //   console.log();
-  // }
-  //console.log('Set timeout end');
+
   setTimeout(() => {
     execute(balancer);
   }, 1000);
